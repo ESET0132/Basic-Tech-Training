e408fbea113a83e52fe95bed1e64b0ccb998f4a7
ShivanshEsya <shivansh.paliwal@esyasoft.com>
2025-09-03 14:36:57 +0530

    Dictionary

--- Python/area.py
+++ Python/area.py
@@ -2,3 +2,10 @@ lenght = int(input("Enter the lenght of rectangle: "))
 breadth = int(input("Enter the breadth of rectangle: "))
 area = lenght * breadth
 print("Area of rectangle is:", area)
+
+student = {
+    "name": "John",
+    "age": 21,
+    "courses": ["Math", "CompSci"]
+}
+print (student)
