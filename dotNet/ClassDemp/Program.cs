9bc5b474d29bf56a2f0f9f8fcd0932651013940f
ShivanshEsya <shivansh.paliwal@esyasoft.com>
2025-09-15 16:08:35 +0530

    Class Demo

---
+++ dotNet/ClassDemp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ClassDemp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            Student stud = new Student();
+            stud.initialize();
+            stud.display();
+            int id = stud.get_student_id();
+            Console.WriteLine("student id from main is: " + id);
+            double totalMarkss = stud.totalMarks(90, 20, 70);
+            Console.WriteLine("Total marks obtained is: " + totalMarkss);
+            double avgM = stud.avgMarks(90, 20, 70);
+            Console.WriteLine("avg marks obtained is: " + avgM);
+        }
+    }
+}
---
+++ dotNet/ClassDemp/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemp
+{
+    internal class Student
+    {
+        int student_id;
+        string student_name;
+        int student_age;
+        string student_address;
+
+
+        public void initialize()
+        {
+            student_id = 101;
+            student_name = "shivansh paliwal";
+            student_age = 21;
+        }
+
+
+        public void display()
+        {
+            Console.WriteLine("student id is: " + student_id);
+            Console.WriteLine("student name is: " + student_name);
+            Console.WriteLine("student age is: " + student_age);
+            Console.WriteLine("student address is: " + student_address);
+        }
+
+
+        public int get_student_id() {
+            return student_id;
+        }
+
+
+        public double totalMarks(int m1, int m2, int m3)
+        {
+            int total = m1 + m2 + m3;
+            return total;
+        }
+
+        public double avgMarks(int m1, int m2, int m3)
+        {
+            int avg = (m1 + m2 + m3) / 3;
+           return avg;
+        }
+
+    }
+}
