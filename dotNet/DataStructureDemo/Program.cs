152a7ced4e413f6e255ab817cefb71d8ebd33ad5
ShivanshEsya <shivansh.paliwal@esyasoft.com>
2025-09-17 12:50:57 +0530

    DatastructureDemo using classes

---
+++ dotNet/DataStructureDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DataStructureDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> student_list = new List<Student>
+            {
+                new Student(101, "Shivansh", 20, "A"),
+                new Student(102, "Lakshay", 21, "B"),
+                new Student(103, "Piyush", 19, "C")
+            };
+
+            foreach (Student s in student_list)
+            {
+                Console.WriteLine($"Student: {s.Name}, ID: {s.Id}, Age: {s.Age}, Grade: {s.Grade}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ dotNet/DataStructureDemo/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureDemo
+{
+    public class Student
+    {
+        public int Id;
+        public string Name;
+        public int Age;
+        public string Grade;
+
+        public Student(int id, string name, int age, string grade)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Grade = grade;
+        }
+    }
+}
