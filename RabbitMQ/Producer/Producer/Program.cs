23ba60821946252789d8af71d43a7b3bdd1e3f7b
ShivanshEsya <shivansh.paliwal@esyasoft.com>
2025-09-24 14:14:17 +0530

    dotNet and RabbitMQ

---
+++ RabbitMQ/Producer/Producer/Program.cs
@@ -0,0 +1,19 @@
+﻿using Producer.Exchanges;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost", VirtualHost = "smart-dev", UserName = "smart-proj", Password = "1234" };
+
+DirectExchange directExchange = new DirectExchange(factory);
+
+Console.WriteLine("Enter messages to send (type 'exit' to quit):");
+
+while (true)
+{
+    string? input = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(input)) continue;
+    if (input.ToLower() == "exit") break;
+
+    await directExchange.SendMessage(input);
+
+
+}
