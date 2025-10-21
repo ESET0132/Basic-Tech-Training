b677a78bf58811e03956880d84bb4763adac3c1a
ShivanshEsya <shivansh.paliwal@esyasoft.com>
2025-10-21 09:50:16 +0530

    linq ex

---
+++ dotNet/Class tasks/Linq_example/Linq_example/Program.cs
@@ -0,0 +1,11 @@
+﻿
+List<string> names = new List<string> { "John", "Jane", "Steve", "Alice", "Bob", "joker", "Jorawar" };
+
+
+IEnumerable<string> query = names.Where(name => name.StartsWith("J"));
+
+
+foreach (string name in query)
+{
+    Console.WriteLine(name);
+}
